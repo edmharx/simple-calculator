b7c132c712a0bb1f70f627036fbdd1505ec74d33
Edmharx Macasinag <edmharxx@gmail.com>
2024-05-10 17:05:13 +0800

    insert value in entry box per button

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ calc = ctk.CTk()
 calc.geometry("360x500")
 calc.title("Calculator")
 
+i=0
+func=""
+
 def insert(value):
     global i
     global func
@@ -53,56 +56,56 @@ button3 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="blac
 button3.place(x=260, y=80)
 
 
-button4 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="7", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button4 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="7", command=lambda:insert("7"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button4.place(x=20, y=160)
 
-button5 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="8", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button5 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="8", command=lambda:insert("8"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button5.place(x=100, y=160)
 
-button6 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="9", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button6 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="9", command=lambda:insert("9"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button6.place(x=180, y=160)
 
-button7 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="-", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button7 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="-", command=lambda:insert("-"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button7.place(x=260, y=160)
 
 
-button8 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="4", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button8 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="4", command=lambda:insert("4"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button8.place(x=20, y=240)
 
-button9 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="5", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button9 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="5", command=lambda:insert("5"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button9.place(x=100, y=240)
 
-button10 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="6", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button10 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="6", command=lambda:insert("6"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button10.place(x=180, y=240)
 
-button11 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="/", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button11 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="/", command=lambda:insert("/"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button11.place(x=260, y=240)
 
 
-button12 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="1", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button12 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="1", command=lambda:insert("1"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button12.place(x=20, y=320)
 
-button13 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="2", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button13 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="2", command=lambda:insert("2"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button13.place(x=100, y=320)
 
-button14 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="3", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button14 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="3", command=lambda:insert("3"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button14.place(x=180, y=320)
 
-button15 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="X", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button15 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="x", command=lambda:insert("x"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button15.place(x=260, y=320)
 
 
 
-button16 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="0", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button16 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="0", command=lambda:insert("0"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button16.place(x=100, y=400)
 
-button17 = ctk.CTkButton(calc, height=80, width=160, hover=True, hover_color="black", text="=", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button17 = ctk.CTkButton(calc, height=80, width=160, hover=True, hover_color="black", text="=", command=lambda:insert("="), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button17.place(x=180, y=400)
 
-button18 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="%", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button18 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text="%", command=lambda:insert("%"), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button18.place(x=180, y=80)
 
-button19 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text=".", command="button_event", corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
+button19 = ctk.CTkButton(calc, height=80, width=80, hover=True, hover_color="black", text=".", command=lambda:insert("."), corner_radius=0, border_color="white", border_width=1, border_spacing=0, font=('Arial',20))
 button19.place(x=20, y=400)
 
 calc.mainloop()
