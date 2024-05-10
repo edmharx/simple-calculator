4aab9a1e06ecb31820c514fcc7f89406ce98c4c3
Edmharx Macasinag <edmharxx@gmail.com>
2024-05-10 10:00:54 +0800

    display menu

--- main.py
+++ main.py
@@ -3,3 +3,15 @@
 #import customtkinter
 #design customtkinter
 #connect buttons for function
+
+def calculator():
+    while True:
+        try:
+            print("Calculator Menu:")
+            print("1. Add")
+            print("2. Subtract")
+            print("3. Multiply")
+            print("4. Divide")
+            print("5. Exit")
+
+            
